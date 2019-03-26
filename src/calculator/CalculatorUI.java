0296be8c787d3474fced380d3bcc3f38b857ed24
Adrian Drechsel (s) <adrian.drechsel@students.fhnw.ch>
2019-03-26 21:18:43 +0100

    First

---
+++ src/calculator/CalculatorUI.java
@@ -0,0 +1,96 @@
+package calculator;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.RowConstraints;
+
+import java.util.ArrayList;
+
+public class CalculatorUI extends GridPane {
+
+    private ArrayList<Button> numberButtons = new ArrayList<>();
+
+    private Button dot;
+
+    private Button plus;
+    private Button minus;
+    private Button multiply;
+    private Button divide;
+
+    private Button plusminus;
+
+    private Button equal;
+    private Button ac;
+
+    private Label display;
+
+    public CalculatorUI() {
+        initializeSelf();
+        initializeControls();
+        layoutControls();
+    }
+
+    private void initializeSelf() {
+        String stylesheet = getClass().getResource("style.css").toExternalForm();
+        getStylesheets().add(stylesheet);
+    }
+
+    private void initializeControls() {
+        for (int i = 0; i < 10; i++) {
+            numberButtons.add(new Button(String.valueOf(i)));
+        }
+
+        dot = new Button(".");
+        plus = new Button("+");
+        minus = new Button("-");
+        multiply = new Button("*");
+        divide = new Button("/");
+
+        plusminus = new Button("+/-");
+        equal = new Button("=");
+        ac = new Button("C");
+
+        display = new Label("0");
+        display.setMaxWidth(Double.MAX_VALUE);
+
+    }
+
+    private void layoutControls() {
+        ColumnConstraints cc = new ColumnConstraints();
+        cc.setHgrow(Priority.ALWAYS);
+        getColumnConstraints().addAll(cc, cc, cc, cc);
+
+        RowConstraints rc = new RowConstraints();
+        rc.setVgrow(Priority.ALWAYS);
+        getRowConstraints().addAll(new RowConstraints(), rc, rc, rc, rc, rc);
+
+        add(display, 0, 0, 4, 1);
+
+        add(ac, 0, 1);
+        add(plusminus, 1, 1);
+        add(divide, 2, 1);
+        add(multiply, 3, 1);
+
+        add(numberButtons.get(7), 0, 2);
+        add(numberButtons.get(8), 1, 2);
+        add(numberButtons.get(9), 2, 2);
+        add(minus, 3, 2);
+
+        add(numberButtons.get(4), 0, 3);
+        add(numberButtons.get(5), 1, 3);
+        add(numberButtons.get(6), 2, 3);
+        add(plus, 3, 3);
+
+        add(numberButtons.get(1), 0, 4);
+        add(numberButtons.get(2), 1, 4);
+        add(numberButtons.get(3), 2, 4);
+        add(equal, 3, 4, 1, 2);
+
+        add(numberButtons.get(0), 0, 5, 2, 1);
+        add(dot, 2, 5);
+    }
+
+}
